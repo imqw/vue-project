824adab472de03fc7f671097940e8a2572177c03
qiuwei <Wcbc0310@126.com>
2020-08-20 10:44:35 +0800

    1 添加测试文件Test.vue 2 添加测试路由

---
+++ src/components/Test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{testMsg}}</h1>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  data () {
+    return {
+      testMsg: '你好,vue'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
