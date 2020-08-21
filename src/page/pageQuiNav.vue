15ce2833d83bbb7414c8455238e5a09a5f63528a
qiuwei <Wcbc0310@126.com>
2020-08-21 14:11:22 +0800

    学习进阶篇--导航栏组件

---
+++ src/page/pageQuiNav.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="pageQuiNav">
+    <qui-nav v-on:navClickEvent="demo1"></qui-nav>
+  </div>
+</template>
+
+<script>
+import quiNav from '../components/quiNav.vue'
+
+export default {
+  name: 'pageQuiNav',
+  components: {
+    'qui-nav': quiNav
+  },
+  methods: {
+    demo1 (item, index) {
+      console.log(item.text + index)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import index from '../page/index'
 import pageQuiButton from '../page/pageQuiButton'
+import pageQuiNav from '../page/pageQuiNav'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/btn',
       name: 'btn',
       component: pageQuiButton
+    },
+    {
+      path: '/nav',
+      name: 'nav',
+      component: pageQuiNav
     }
   ]
 })
